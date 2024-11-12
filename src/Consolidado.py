29bdf53e319cfd35b06ba9fa4636b0a1176757d0
zulu <cesarzulu82@gmail.com>
2024-11-12 08:18:56 -0500

    implementacion clase Consolidado

--- src/Consolidado.py
+++ src/Consolidado.py
@@ -1,1 +1,9 @@
 class Consolidado:
+    def __init__(self):
+        self.datosConsolidado = {}
+
+    def agregar_dato(self,llave,nuevo_dato):
+        self.datosConsolidado[llave] = nuevo_dato
+
+    def buscar_dato(self,llave):
+        return self.datosConsolidado[llave]
