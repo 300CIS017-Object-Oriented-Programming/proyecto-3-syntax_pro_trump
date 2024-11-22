631a58a152ec164b9583a1ad6c30e3f76f59c503
Mateo <monroymateo1@gmail.com>
2024-11-22 09:45:12 -0500

    MOD: Ya casi se termina de hacer el procesar datos solo falta eliminar unas columnas de años del df final

--- src/SNIESController.py
+++ src/SNIESController.py
@@ -26,31 +26,57 @@ class SNIESController:
         try:
             # Generar lista de años
             anios = self.generar_anios_busqueda(anio1, anio2)
-            self.df = lector.leer_archivo(LISTA_DIRECCIONES[0]+anios[0]+".xlsx", palabra_clave, False)
             # Iterar por los archivos adicionales
             flag = True
+            flag2 = True
+
             for anio in anios:
+                #FIXME: poner bandera para solo hacer el merge con el primer archivo de un año
                 for direccion in LISTA_DIRECCIONES:
-                    ruta_archivo = direccion + anio + ".xlsx"
+                    if flag2:
+                        # Leer el primer archivo
+                        self.df = lector.leer_archivo(LISTA_DIRECCIONES[0] + anios[0] + ".xlsx", palabra_clave, False)
 
-                    # Leer archivo con la función 'lector'
-                    df_filtrado = lector.leer_archivo(ruta_archivo, palabra_clave, True)
+                        # Convertir columnas clave a tipo string
+                        self.df["CÓDIGO SNIES DEL PROGRAMA"] = self.df["CÓDIGO SNIES DEL PROGRAMA"].astype(str)
+                        self.df["SEMESTRE"] = self.df["SEMESTRE"].astype(str)
+                        self.df["AÑO"] = self.df["AÑO"].astype(str)
 
-                    if not flag:
-                        # Caso especial: primer año (primer elemento de 'anios')
-                        if anio == anios[0]:  # Comparar con el primer año de la lista
-                            # Agregar solo la última columna
-                            ultima = df_filtrado.columns[-1]  # Nombre de la última columna
-                            ultima_columna = df_filtrado[[ultima]]  # Seleccionar la última columna
-                            self.df = pd.concat([self.df, ultima_columna], axis=1)
-                        else:
-                            # Concatenar todo el DataFrame filtrado
-                            self.df = pd.concat([self.df, df_filtrado], axis=1)
+                        flag2 = False
                     else:
-                        # Flag activado: solo para el primer archivo
-                        flag = False
+                        # Leer archivo para el año actual
+                        ruta_archivo = direccion + anio + ".xlsx"
+                        df_filtrado = lector.leer_archivo(ruta_archivo, palabra_clave, True)
+
+                        # Convertir columnas clave a tipo string
+                        df_filtrado["CÓDIGO SNIES DEL PROGRAMA"] = df_filtrado["CÓDIGO SNIES DEL PROGRAMA"].astype(str)
+                        df_filtrado["SEMESTRE"] = df_filtrado["SEMESTRE"].astype(str)
+                        df_filtrado["AÑO"] = df_filtrado["AÑO"].astype(str)
+
+                        # Realizar la fusión
+                        if anio == anios[0]:
+                            self.df = pd.merge(
+                                self.df, df_filtrado,
+                                on=["CÓDIGO SNIES DEL PROGRAMA", "SEMESTRE", "AÑO"],
+                                how="left",
+                                suffixes=("", f"_{anio}")
+                            )
+                        else:
+                            self.df = pd.merge(
+                                self.df, df_filtrado,
+                                on=["CÓDIGO SNIES DEL PROGRAMA", "SEMESTRE"],
+                                how="left",
+                                suffixes=("", f"_{anio}")
+                            )
+
+                        # Renombrar las columnas duplicadas (agregar el año como sufijo)
+                        columnas_duplicadas = [col for col in self.df.columns if col.endswith(f"_{anio}")]
+                        for col in columnas_duplicadas:
+                            nuevo_nombre = f"{col.split('_')[0]}_{anio}"  # Nombre base + año
+                            self.df.rename(columns={col: nuevo_nombre}, inplace=True)
 
             return self.df
+
 
         except Exception as e:
             print(f"Error al procesar los datos: {e}")
--- src/main.py
+++ src/main.py
@@ -5,15 +5,15 @@ from src.GestorArchivos import GestorArchivos
 import pandas as pd
 controller = SNIESController()
 leer = GestorArchivos()
-#df = controller.procesar_datos(2020, 2021, "ZOO")
+df = controller.procesar_datos(2020, 2021, "ZOO")
 #print("Sali")
 #df = pd.read_excel("D:/ProyectoPOO/proyecto-3-syntax_pro_trump/docs/inputs/admitidos2020.xlsx")
 #print("Ya sali")
 #df.to_excel("Prueba.xlsx", index=False)
 #print("Termine")
 #controller.mostrar()
-df1 = leer.leer_archivo("D:/ProyectoPOO/proyecto-3-syntax_pro_trump/docs/inputs/admitidos2020.xlsx", "ZOO", False)
-df2 = leer.leer_archivo("D:/ProyectoPOO/proyecto-3-syntax_pro_trump/docs/inputs/admitidos2021.xlsx", "ZOO", False)
+#df1 = leer.leer_archivo("D:/POO/proyecto-3-syntax_pro_trump/docs/inputs/admitidos2020.xlsx", "ZOO", False)
+#df2 = leer.leer_archivo("D:/POO/proyecto-3-syntax_pro_trump/docs/inputs/graduados2020.xlsx", "ZOO", True)
 #common_columns = df1.columns.intersection(df2.columns)
 #df2_unique = df2.merge(df1, on=list(common_columns), how="left", indicator=True)
 #df2_unique = df2_unique[df2_unique["_merge"] == "left_only"].drop(columns=["_merge"])
@@ -21,11 +21,12 @@ df2 = leer.leer_archivo("D:/ProyectoPOO/proyecto-3-syntax_pro_trump/docs/inputs/
 #ultima =  df2.columns[-1]
 #ultima_columna = df2[ultima]
 #result = pd.concat([df1, ultima_columna], axis=1)
+#ultima = df2.columns[-1]  # Nombre de la última columna
+#ultima_columna = df2[["CÓDIGO SNIES DEL PROGRAMA", "SEMESTRE",ultima]]
+
+#df = pd.merge(df1, ultima_columna, on=["CÓDIGO SNIES DEL PROGRAMA", "SEMESTRE"], how="left")
 
 
-#df = pd.merge(df1, df2, on="CÓDIGO SNIES DEL PROGRAMA", how="left")
-
-
-df1.to_excel("Prueba1.xlsx", index=False)
-df2.to_excel("Prueba.xlsx", index=False)
+df.to_excel("Prueba5.xlsx", index=False)
+#df2.to_excel("Prueba.xlsx", index=False)
 
